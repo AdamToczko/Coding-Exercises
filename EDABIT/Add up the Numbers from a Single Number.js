67cd40c23b03e9e7630a3b3fea3a1a563a845af3
Adam Toczko <adamtoczko80@gmail.com>
2019-11-27 19:14:46 +0100

    added new solution for edabit 	new file:   EDABIT/Add up the Numbers from a Single Number.js

---
+++ EDABIT/Add up the Numbers from a Single Number.js
@@ -0,0 +1,16 @@
+// Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+// Examples
+// addUp(4) ➞ 10
+
+// addUp(13) ➞ 91
+
+// addUp(600) ➞ 180300
+
+function addUp(num) {
+    if (num <= 1) {
+      return num;
+    } else {
+      return num + addUp(num - 1);
+    }
+  }

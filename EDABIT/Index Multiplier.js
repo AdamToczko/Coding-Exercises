cea33f9b3aff49dcb2ff42acae27ba9b90449bc7
Adam Toczko <adamtoczko80@gmail.com>
2019-11-28 19:56:34 +0100

    edabit challenge

---
+++ EDABIT/Index Multiplier.js
@@ -0,0 +1,16 @@
+// https://edabit.com/challenge/3Efavz8YmSBia4p8s
+
+// Return the sum of all items in an array, where each item is multiplied by its index (zero-based). For empty arrays, return 0.
+
+// Examples
+// indexMultiplier([1, 2, 3, 4, 5]) ➞ 40
+// // (1*0 + 2*1 + 3*2 + 4*3 + 5*4)
+
+// indexMultiplier([-3, 0, 8, -6]) ➞ -2
+// // (-3*0 + 0*1 + 8*2 + -6*3)
+// Notes
+// All items in the array will be integers.
+
+function indexMultiplier(arr) {
+	return arr.map((e,i) => e * i).reduce((sum, current) => sum + current,0)
+}

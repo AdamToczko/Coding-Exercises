0fda86f5284d39c783f23b5e4f271742ca510e9f
Adam Toczko <adamtoczko80@gmail.com>
2020-05-25 08:09:34 +0200

    kata 7kyu completed

---
+++ CODEWARS/Greet Me.js
@@ -0,0 +1,14 @@
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+
+var greet = function(name) {
+    return( 
+    "Hello " +
+    name.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+    + "!")
+};
+
